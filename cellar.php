b69215ab4760d43c33fe5df944071a576148ae25
Nicholas Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-03-09 16:47:31 +1000

    remove reference to hard-added php-qrcode

--- cellar.php
+++ cellar.php
@@ -1,5 +1,3 @@
 <?php
 header('X-cellar-Version: 0.1a-prealpha');
 $_CELLAR[] = array();
-
-require_once (__DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'chillerlan' . DIRECTORY_SEPARATOR . 'qrcode.php');
