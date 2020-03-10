d4b9d965a6133e2570c7b474d47e10876be0e30f
Nicholas Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-03-10 12:08:09 +1000

    add db logic to cellar.php

--- cellar.php
+++ cellar.php
@@ -1,3 +1,17 @@
 <?php
 header('X-cellar-Version: 0.1a-prealpha');
 $_CELLAR[] = array();
+
+const DB_HOST = 'localhost:3306';
+
+const DB_USER = 'root';
+
+const DB_PASS = '';
+
+const DB_NAME = 'cellar';
+
+$_CELLAR['database'] = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if (mysqli_connect_errno()) {
+    printf('Connect failed: %s' . PHP_EOL, mysqli_correct_error());
+    exit();
+}
