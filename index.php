73feace9575588c45d37bc80a351fc8b7bbd6028
Nicholas Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-03-10 12:08:26 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once (__DIR__ . DIRECTORY_SEPARATOR . 'cellar.php');
+$_CELLAR['page_title'] = 'home';
+require_once (__DIR__ . DIRECTORY_SEPARATOR . 'header.php');
+?>
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+<h2>cellar</h2>
+</main>
+<?php
+require_once (__DIR__ . DIRECTORY_SEPARATOR . 'footer.php');
+?>
