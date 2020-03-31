701a6842452fed0a8b84c2166db0636a1a65e61a
Nicholas Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-03-31 15:19:59 +1000

    Fix mistakenly named variables

--- wineries.php
+++ wineries.php
@@ -8,26 +8,26 @@ if (isset($_GET['id'])) {
     $winery = $_GET['id'];
     $winery = mysqli_real_escape_string($_CELLAR['database'], $winery);
     $winery = $_CELLAR['database']->query('SELECT * FROM `winery` WHERE `id`=\'' . $winery . '\';');
-    $_LOCATION = $winery->fetch_assoc();
+    $_WINERY = $winery->fetch_assoc();
     unset($winery);
 }
 ?>
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
 	<h2>Wineries</h2>
 <?php
-if (isset($_LOCATION)) {
-    echo ('<h4>' . $_LOCATION['name'] . '</h4>');
+if (isset($_WINERY)) {
+    echo ('<h4>' . $_WINERY['name'] . '</h4>');
     // location
-    $location = $_LOCATION['location'];
+    $location = $_WINERY['location'];
     $location = mysqli_real_escape_string($_CELLAR['database'], $location);
     $location = $_CELLAR['database']->query('SELECT * FROM `location` WHERE `id`=\'' . $location . '\';');
-    $_LOCATION['location'] = $location->fetch_assoc();
+    $_WINERY['location'] = $location->fetch_assoc();
     unset($location);
     ?>
     <br>
 	<?php
-    if (isset($_LOCATION['logo'])) {
-        echo ('<img src="' . $_LOCATION['logo'] . '" style="width: 100%; max-width: 120px;" /><br><br>');
+    if (isset($_WINERY['logo'])) {
+        echo ('<img src="' . $_WINERY['logo'] . '" style="width: 100%; max-width: 120px;" /><br><br>');
     }
     ?>
 	
@@ -35,7 +35,7 @@ if (isset($_LOCATION)) {
     if ($_CELLAR['gmaps_embed_enabled']) {
         ?>
 	<iframe width="65%" height="450" frameborder="0" style="border: 0"
-		src="https://www.google.com/maps/embed/v1/place?q=<?= htmlspecialchars($_LOCATION['location']['address']); ?>&maptype=<?= htmlspecialchars($_CELLAR['gmaps_embed_view']); ?>&key=<?= htmlspecialchars($_CELLAR['gmaps_embed_apikey']); ?>"
+		src="https://www.google.com/maps/embed/v1/place?q=<?= htmlspecialchars($_WINERY['location']['address']); ?>&maptype=<?= htmlspecialchars($_CELLAR['gmaps_embed_view']); ?>&key=<?= htmlspecialchars($_CELLAR['gmaps_embed_apikey']); ?>"
 		allowfullscreen></iframe>
 	<br>
 	<?php
@@ -43,15 +43,15 @@ if (isset($_LOCATION)) {
         ?>
         <p>
 		<a
-			href="https://google.com/maps/search/<?= htmlspecialchars($_LOCATION['location']['address']); ?>">view
+			href="https://google.com/maps/search/<?= htmlspecialchars($_WINERY['location']['address']); ?>">view
 			on map</a>
 	</p>
         <?php
     }
     ?>
-    <small><a
-		href="locations.php?id=<?= $_LOCATION['location']['id']; ?>">view
-			location in cellar</a></small>
+    <small><a href="locations.php?id=<?= $_WINERY['location']['id']; ?>">view
+			location in cellar</a></small><br> <br>
+
 <?php
 } else {
     $wineries = $_CELLAR['database']->query('SELECT `id`,`name` FROM `winery`;');
