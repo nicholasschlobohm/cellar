4df1f125ce86949022607357e735d2b74ec31c16
Nicholas Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-03-31 15:03:19 +1000

    Load confkeys into $_CELLAR var

--- cellar.php
+++ cellar.php
@@ -20,3 +20,8 @@ if (mysqli_connect_errno()) {
     printf('Connect failed: %s' . PHP_EOL, mysqli_correct_error());
     exit();
 }
+$config_dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
+$_CELLAR['gmaps_embed_enabled'] = filter_var(@file_get_contents($config_dir . 'GoogleMapsEmbedEnabled'), FILTER_VALIDATE_BOOLEAN);
+$_CELLAR['gmaps_embed_apikey'] = @file_get_contents($config_dir . 'GoogleMapsEmbedApiKey');
+$_CELLAR['gmaps_embed_view'] = @file_get_contents($config_dir . 'GoogleMapsEmbedView');
+unset($config_dir);
