030334ff1e1f7c386a3166ede3e973ddfeafe9a8
Nicholas Schlobohm <nschlobohm@willowgreengroup.com.au>
2020-03-31 15:03:06 +1000

    Mistakenly-named variables fixed

--- catalogues.php
+++ catalogues.php
@@ -5,56 +5,55 @@ require_once (__DIR__ . DIRECTORY_SEPARATOR . 'header.php');
 
 // page logic
 if (isset($_GET['id'])) {
-    $catalogue = $_GET['id'];
-    $catalogue = mysqli_real_escape_string($_CELLAR['database'], $catalogue);
-    $catalogue = $_CELLAR['database']->query('SELECT * FROM `catalogue` WHERE `id`=\'' . $catalogue . '\';');
-    $_CATALOGUE = $catalogue->fetch_assoc();
-    unset($catalogue);
+    $location = $_GET['id'];
+    $location = mysqli_real_escape_string($_CELLAR['database'], $location);
+    $location = $_CELLAR['database']->query('SELECT * FROM `catalogue` WHERE `id`=\'' . $location . '\';');
+    $_LOCATION = $location->fetch_assoc();
+    unset($location);
 }
 ?>
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
 	<h2>Catalogues</h2>
 <?php
 
-if (isset($_CATALOGUE)) {
-    echo ('<h4>' . $_CATALOGUE['title'] . '</h4>');
+if (isset($_LOCATION)) {
+    echo ('<h4>' . $_LOCATION['title'] . '</h4>');
     // winery
-    $winery = $_CATALOGUE['winery'];
-    $winery = mysqli_real_escape_string($_CELLAR['database'], $winery);
-    $winery = $_CELLAR['database']->query('SELECT * FROM `winery` WHERE `id`=\'' . $winery . '\';');
-    $_CATALOGUE['winery'] = $winery->fetch_assoc();
-    unset($winery);
+    $location = $_LOCATION['winery'];
+    $location = mysqli_real_escape_string($_CELLAR['database'], $location);
+    $location = $_CELLAR['database']->query('SELECT * FROM `winery` WHERE `id`=\'' . $location . '\';');
+    $_LOCATION['winery'] = $location->fetch_assoc();
+    unset($location);
 
     ?>
     <p>
 		catalogue from <a
-			href="wineries.php?id=<?= $_CATALOGUE['winery']['id'] ?>"><?= $_CATALOGUE['winery']['name'] ?></a>
+			href="wineries.php?id=<?= $_LOCATION['winery']['id'] ?>"><?= $_LOCATION['winery']['name'] ?></a>
 	</p>
 
-	<small><a href="upload/catalogue/<?= $_CATALOGUE['id'] ?>.pdf"
+	<small><a href="upload/catalogue/<?= $_LOCATION['id'] ?>.pdf"
 		target="_blank">click here for raw file</a></small>
-	<iframe src="upload/catalogue/<?= $_CATALOGUE['id'] ?>.pdf#toolbar=0"
+	<iframe src="upload/catalogue/<?= $_LOCATION['id'] ?>.pdf#toolbar=0"
 		width="100%" height="750px"></iframe>
 	
     <?php
 } else {
-    $catalogues = $_CELLAR['database']->query('SELECT `id`,`title` FROM `catalogue`;');
-    $_CATALOGUES = array();
-    while ($row = mysqli_fetch_assoc($catalogues)) {
-        $_CATALOGUES[] = $row;
+    $wineries = $_CELLAR['database']->query('SELECT `id`,`title` FROM `catalogue`;');
+    $_WINERIES = array();
+    while ($row = mysqli_fetch_assoc($wineries)) {
+        $_WINERIES[] = $row;
     }
-    unset($catalogues);
+    unset($wineries);
 
-    foreach ($_CATALOGUES as $catalogue) {
+    foreach ($_WINERIES as $location) {
         ?>
-        <a href="?id=<?= $catalogue['id'] ?>"><?= $catalogue['title'] ?></a>
+        <a href="?id=<?= $location['id'] ?>"><?= $location['title'] ?></a>
 	<br>
         <?php
     }
 }
 
 ?>
-
 
 </main>
 <?php
